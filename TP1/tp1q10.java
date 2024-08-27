6112547f178f570bd97075166bd229b86428ecea
otavioaugustoafm <otavioaugustoafm@gmail.com>
2024-08-27 14:11:18 -0300

    TP1

--- TP1/tp1q10.java
+++ TP1/tp1q10.java
@@ -1,37 +1,33 @@
-import java.util.Scanner;
 
 public class tp1q10 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        palindromo(scanner);
-        scanner.close();
-    }
-    public static void palindromo(Scanner scanner) {
-        String frase = "Inicio";
-        int tamanho, index1, index2;
-        String fim = "FIM";
-        frase = scanner.nextLine();
-        if(frase.equals(fim)) {
-            return;
-        } else {
-            boolean resp = true;
-            tamanho = frase.length();
-            index1 = 0;
-            index2 = tamanho - 1;
-            for(int i = 0; i < tamanho / 2; i++) {
-                if(frase.charAt(index1) != frase.charAt(index2)) {
-                    resp = false;
-                    break;
-                } 
-                index1++;
-                index2--;
+        boolean resp = true;
+        while(true) {
+            String frase = MyIO.readLine();
+            int tam = frase.length();
+            if(frase.equals("FIM")) {
+                break;
             }
-            if( resp == true) {
-                System.out.println("SIM");
+            resp = palindromo(frase,0, resp, tam);
+            if(resp == true) {
+                MyIO.print("SIM");
             } else {
-                System.out.println("NAO");
+                MyIO.print("NAO");
+            }
+            MyIO.print("\n");
+        }
+    }
+    public static boolean palindromo(String frase, int i, boolean resp, int tam) {
+        if(i == frase.length()){
+            return resp;
+        } else {
+            if (frase.charAt(i) == frase.charAt(tam - 1)) {
+                resp = true;
+                return palindromo(frase,i + 1, resp, tam - 1);
+            } else {
+                resp = false;
+                return resp;
             }
         }
-        palindromo(scanner);
     }
 }
