b8df768e6dc8f1a5ba8c481b3d1a64baff044b8b
otavioaugustoafm <otavioaugustoafm@gmail.com>
2024-10-22 17:24:36 -0300

    Quicksort

---
+++ Estudos/Ordenação/quicksort.java
@@ -0,0 +1,43 @@
+//DIVISAO E CONQUISTA
+//BASEA-SE EM IR DIVIDINDO O ARRAY ALVO EM 2 SUCESSIVAMENTE
+//E IR ORDENANDO AOS POUCOS ATÉ CONSEGUIR
+
+import java.util.*;
+import random;
+
+public class Data {
+    int dia;
+    int mes;
+    int ano;
+
+    public Data() {
+        this.dia = 0;
+        this.mes = 0;
+        this.ano = 0;
+    }
+    
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public void mostrarData() {
+        System.out.println(dia + "/" + mes + "/" + ano);
+    }
+}
+
+public class Livro {
+    String nome;
+    String autor;
+    String lingua;
+    Data data;
+
+    public 
+}
+
+public class quicksort {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
