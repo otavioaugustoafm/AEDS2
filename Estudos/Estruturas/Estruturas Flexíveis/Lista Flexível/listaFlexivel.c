4007941be8277b93373505ad8b2a946f409ebf11
otavioaugustoafm <otavioaugustoafm@gmail.com>
2024-11-10 22:47:56 -0300

    ajustes

--- Estudos/Estruturas/Estruturas Flexíveis/Lista Flexível/listaFlexivel.c
+++ Estudos/Estruturas/Estruturas Flexíveis/Lista Flexível/listaFlexivel.c
@@ -42,25 +42,65 @@ void inserirFim(Lista* lista, char especie[]) {
     lista->n_elementos++;
 }
 
+void inserirInicio(Lista* lista, char especie[]){
+    Arvore* arvore = (Arvore*)malloc(sizeof(Arvore));
+    construtorArvore(arvore);
+    strcpy(arvore->especie, especie);
+    if(lista->primeiro == NULL) {
+        lista->primeiro = arvore;
+        lista->ultimo = arvore; 
+    } else {
+        Arvore* tmp = lista->primeiro;
+        lista->primeiro = arvore;
+        arvore->prox = tmp;
+        tmp->ant = arvore;   
+    }
+    lista->n_elementos++;
+}
+
 void removerFim(Lista* lista) {
     if (lista->ultimo == NULL) { 
         printf("A lista já está vazia.\n");
         return;
     }
-    Arvore* ref = lista->ultimo;
     if(lista->primeiro == lista->ultimo) {
         lista->primeiro = NULL;
         lista->ultimo = NULL;
     } else {
+        Arvore* tmp = lista->ultimo;
         lista->ultimo = lista->ultimo->ant;
         lista->ultimo->prox = NULL;
+        free(tmp);
     }
-    free(ref);
+    lista->n_elementos--;
 }
 
+void removerInicio(Lista* lista) {
+    if(lista->primeiro == NULL) {
+        printf("Lista vazia\n");
+        return;
+    }
+    if(lista->primeiro == lista->ultimo) {
+        lista->ultimo = NULL;
+        lista->primeiro = NULL;
+    } else {
+        Arvore* tmp = lista->primeiro;
+        lista->primeiro = lista->primeiro->prox;
+        lista->primeiro->ant = NULL;
+        free(tmp);
+    }
+    lista->n_elementos--;
+}
+
+
+
 void mostrar(Lista* lista) {
-    printf("A lista esta assim: \n");
     Arvore* ref = lista->primeiro;
+    if(lista->primeiro == NULL) {
+        printf("Lista vazia\n");
+        return;
+    }
+    printf("A lista esta assim: \n");
     for(int i = 0; i < lista->n_elementos; i++) {
         printf("%s | ", ref->especie);
         if(ref->prox == NULL) {
@@ -72,7 +112,7 @@ void mostrar(Lista* lista) {
 }
 
 void menu() {
-    printf("=====Vamos trabalhar com uma Lista Flexivel=====\n1 - Adicionar arvore no fim da lista\n2 - Remover elemento no fim da lista\n6 - Mostrar\n0 - Sair\nQual opcao deseja?\n");
+    printf("=====Vamos trabalhar com uma Lista Flexivel=====\n1 - Adicionar elemento no fim da lista\n2 - Adicionar elemento no inicio da lista\n3 - Remover elemento no fim da lista\n4 - Remover elemento no inicio da lista\n6 - Mostrar\n0 - Sair\nQual opcao deseja?\n");
 }
 
 int main() {
@@ -82,16 +122,30 @@ int main() {
     while(op != 0) {
         menu();
         scanf("%d", &op);
+        getchar();
         switch(op) {
             case 1: {
                 char string[31];
                 printf("Qual a especie? \n");
-                scanf("%s", string);
+                fgets(string, 31, stdin);
+                string[strcspn(string, "\n")] = '\0';
                 inserirFim(&lista, string);
                 break;
             }
             case 2: {
+                char string[31];
+                printf("Qual a especie? \n");
+                fgets(string, 31, stdin);
+                string[strcspn(string, "\n")] = '\0';
+                inserirInicio(&lista, string);
+                break;
+            }
+            case 3: {
                 removerFim(&lista);
+                break;
+            }
+            case 4: {
+                removerInicio(&lista);
                 break;
             }
             case 6: {
