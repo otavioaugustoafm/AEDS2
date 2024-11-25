c37f3784e56da058db89caf95bb727beb822c2d2
Otávio Augusto A. F. Monteiro <90585162+otavioaugustoafm@users.noreply.github.com>
2024-11-25 11:41:49 -0300

    Create arvore.java

---
+++ Estudos/Estruturas/Árvore/arvore.java
@@ -0,0 +1,23 @@
+class Arvore {
+    No raiz;
+
+    public Arvore() {
+        this.raiz = null;
+    }
+
+    void Inserir() {
+
+    }
+}
+
+class No {
+    No dir;
+    No esq;
+    char elemento;
+}
+
+public class arvore {
+    public static void main(String[] args) {
+        
+    }
+}
