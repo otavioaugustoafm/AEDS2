60327599cd037551723e25e19cec6a41ce722eb6
otavioaugustoafm <otavioaugustoafm@gmail.com>
2024-12-03 17:25:27 -0300

    arvorenormal

--- Estudos/Estruturas/Árvore/arvore.java
+++ Estudos/Estruturas/Árvore/arvore.java
@@ -32,23 +32,68 @@ class Agenda {
         Celula nova = new Celula(contato);
         Celula tmp = atual.primeiro;
 
-        if(tmp.prox == null || tmp.prox.contato.nome.compareTo(contato.nome) > 0) {
-            nova.prox = tmp.prox;
-            tmp.prox = nova;
-            if(nova.prox == null) {
-                atual.ultimo = nova;
-            } else {
-                while(tmp.prox != null && tmp.prox.contato.nome.compareTo(contato.nome) < 0) {
-                    tmp = tmp.prox;
-                }
-                nova.prox = tmp.prox;
-                tmp.prox = nova;
-                if(nova.prox == null) {
-                    atual.ultimo = nova;
-                }
-            }
+        while(tmp.prox != null && tmp.prox.contato.nome.compareTo(contato.nome) < 0) {
+            tmp = tmp.prox;
+        }
+        nova.prox = tmp.prox;
+        tmp.prox = nova;
+        
+        if (nova.prox == null) {
+            atual.ultimo = nova;
         }
     }
+    
+    void percorrerEmOrdem() {
+        percorrerEmOrdemRecursivo(this.raiz);
+    }
+
+    void percorrerEmOrdem(String cpf) {
+        percorrerEmOrdemRecursivo(this.raiz, cpf);
+    }
+
+    void mostrarLista(No atual) {
+        Celula tmp = atual.primeiro.prox;
+        while(tmp != null) {
+            System.out.println("Nome: " + tmp.contato.nome + "| Telefone: " + tmp.contato.telefone + "| E-mail: " + tmp.contato.email + "| CPF: " + tmp.contato.cpf);
+            tmp = tmp.prox;
+        }
+    }
+
+    void percorrerEmOrdemRecursivo(No atual){
+        if(atual != null) {
+            percorrerEmOrdemRecursivo(atual.esq);
+            System.out.println("Letra do nó: " + atual.letra);
+            mostrarLista(atual);
+            percorrerEmOrdemRecursivo(atual.dir);
+        }
+    }
+
+    void percorrerEmOrdemRecursivo(No atual, String cpf){
+        if(atual != null) {
+            percorrerEmOrdemRecursivo(atual.esq);
+            removerLista(atual, cpf);
+            percorrerEmOrdemRecursivo(atual.dir);
+        }
+    }
+
+    void removerLista(No atual, String cpf) {
+        Celula tmp = atual.primeiro;
+        Celula tmp2 = atual.primeiro.prox;
+        while(tmp2 != null) {
+            if(tmp2.contato.cpf.equals(cpf)) {
+                System.out.println(tmp2.contato.cpf + " será excluído!");
+                tmp.prox = tmp2.prox;
+                if(tmp2.prox == null) {
+                    atual.ultimo = tmp;
+                }
+                tmp2 = null;
+                break;
+            }
+            tmp = tmp2;
+            tmp2 = tmp2.prox;
+        }
+    }
+    
 }
 
 class No {
@@ -99,7 +144,6 @@ class Contato {
 public class arvore {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Contato contato = new Contato();
         Agenda agenda = new Agenda();
         menu();
         int op = scanner.nextInt(); 
@@ -107,6 +151,7 @@ public class arvore {
         while(op != 0) {
             switch(op) {
                 case 1: {
+                    Contato contato = new Contato();
                     System.out.println("Digite o nome: ");
                     contato.nome = scanner.nextLine();
                     System.out.println("Digite o telefone: ");
@@ -116,13 +161,31 @@ public class arvore {
                     System.out.println("Digite o CPF: ");
                     contato.cpf = scanner.nextLine();
                     agenda.inserir(contato);
-                    System.out.println("");
+                    System.out.println("Contato inserido com sucesso!");
                     break;
                 }
+                case 2: {
+                    Contato contato = new Contato();
+                    System.out.println("Digite o CPF: ");
+                    contato.cpf = scanner.nextLine();
+                    agenda.removerLista(agenda.raiz, contato.cpf);
+                    break;
+                }
+                case 3: {
+                    agenda.percorrerEmOrdem();
+                    break;
+                }
+
+                default: {
+                    System.out.println("Opção inválida.");
+                    break;
+                }
+
             }
 
             menu();
             op = scanner.nextInt();
+            scanner.nextLine();
         }
         scanner.close();
     }
